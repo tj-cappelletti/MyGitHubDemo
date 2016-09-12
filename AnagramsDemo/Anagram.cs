dd5f4ec1981fb61be8496c835ac83e094aafc110
Thomas J. Cappelletti <tcappelletti@webstormtechnologies.com>
2016-09-12 08:11:19 -0400

    Adding project

---
+++ AnagramsDemo/Anagram.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramsDemo
+{
+    public class Anagram
+    {
+        private readonly String filePath;
+
+        public Anagram()
+        {
+            filePath = ConfigurationManager.AppSettings["WordListFile"];
+        }
+
+        public List<String> FindAnagrams(String word)
+        {
+            List<String> anagrams = new List<String>();
+            Dictionary<char, int> wordLetters = GenerateCharterDictionary(word);
+
+            using (StreamReader streamReader = File.OpenText(filePath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    String entry = streamReader.ReadLine();
+
+                    if (entry.Length == word.Length)
+                    {
+                        Dictionary<char, int> entryLetters = GenerateCharterDictionary(entry);
+
+                        if (wordLetters.Count == entryLetters.Count)
+                        {
+                            bool match = true;
+
+                            foreach (KeyValuePair<char, int> kvp in wordLetters)
+                            {
+                                if (!entryLetters.ContainsKey(kvp.Key) || entryLetters[kvp.Key] != kvp.Value)
+                                    match = false;
+                            }
+
+                            if (match)
+                                anagrams.Add(entry);
+                        }
+                    }
+                    else
+                        continue;
+                }
+            }
+
+            return anagrams;
+        }
+
+        private Dictionary<char, int> GenerateCharterDictionary(String word)
+        {
+            string lowerWord = word.ToLower();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char character in lowerWord)
+            {
+                if (dictionary.ContainsKey(character))
+                    dictionary[character]++;
+                else
+                    dictionary.Add(character, 1);
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ AnagramsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+            List<String> anagrams = anagram.FindAnagrams("pots");
+
+            foreach(var entry in anagrams)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
